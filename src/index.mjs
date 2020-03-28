cf86217b631030aef2afcd82b51398a0612fcdab
Sosuke Suzuki <aosukeke@gmail.com>
2020-03-29 03:49:36 +0900

    initial commit

---
+++ src/index.mjs
@@ -0,0 +1,7 @@
+import { module1 } from "./module1";
+
+function call() {
+  module1();
+}
+
+call();
---
+++ src/module1.js
@@ -0,0 +1,6 @@
+import { module2 } from "./module2";
+
+export function module1() {
+  module2();
+  console.log("HELLO, MODULE!");
+}
